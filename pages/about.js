2ff95e18567e19fbfd799323f47f6f762432323b
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-10 00:37:16 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -15,6 +15,7 @@ export default function Home() {
         </h1>
     
         <div>
+            <p></p>
             <p>
             I am a Computer Scientist currently employed at Ridgeline Manufacturing as a Project and Operations Manager
             working to automate their reactor equipment and increase production efficiency.
@@ -23,7 +24,7 @@ export default function Home() {
             In 2020, I was the project manager for a university based start-up with the goal of implementing a cloud
             based portal for QUT. From 2018 to 2019, I was responsible for developing business applications and supporting
             the production environment of a large food processing plant. In 2016, I completed a Diploma of Software Development
-            at Tafe Queensland and was employed as a Computer Technician from 2012 to 2015.
+            at Tafe Queensland and from 2012 to 2015 was employed as a Computer Technician.
             </p>
             <p>
             Upon completion of my degree I plan to have a solid background of understanding in a range of development
