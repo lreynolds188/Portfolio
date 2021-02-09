d39c673015dd28c1940f0f4a0de14510beeea5f2
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-09 23:08:46 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,10 @@ export default function Home() {
       <main className={styles.main}>
         <div className="flex flex-wrap justify-center">
           <div className="w-6/12 sm:w-4/12 px-4">
-            <img src="/profile.jpg" alt="..." className="shadow-lg rounded-full max-w-full h-auto align-middle border-none" />
+            <img src="/profile.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
           </div>
         </div>
+
 
 
 
