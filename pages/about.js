c3d8c7427e43ef0a59dbf6928bf048dd1bfcd12a
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-09 22:31:02 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -15,12 +15,9 @@ export default function Home() {
         </h1>
     
         <div>
-            <p>
-              I am a Computer Scientist currently employed at Ridgeline Manufacturing as a Project and Operations Manager working to automate their reactor equipment and increase production efficiency.
-
-              In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud based portal for QUT. From 2018 to 2019 I was responsible for developing business applications and supporting the production environment of a large food processing plant. I completed a Diploma of Software Development at Tafe Queensland in 2016 and was employed as a Computer Technician from 2012 to 2015.
-
-              Upon completion of final course in my degree I plan to have a solid background in a range of development languages and techniques, with a sound understanding of cyber security and artificial intelligence applications. My hope is to work advancing the functionality, affordability, and sustainability of futuristic applications of technology.
+            <p>I am a Computer Scientist currently employed at Ridgeline Manufacturing as a Project and Operations Manager working to automate their reactor equipment and increase production efficiency.</p>
+            <p>In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud based portal for QUT. From 2018 to 2019 I was responsible for developing business applications and supporting the production environment of a large food processing plant. I completed a Diploma of Software Development at Tafe Queensland in 2016 and was employed as a Computer Technician from 2012 to 2015.</p>
+            <p>Upon completion of final course in my degree I plan to have a solid background in a range of development languages and techniques, with a sound understanding of cyber security and artificial intelligence applications. My hope is to work advancing the functionality, affordability, and sustainability of futuristic applications of technology.</p>
             </p>
         </div>
       </main>
