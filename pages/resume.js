b3ea8da83c40f72578ecf4280dd23198e645f8e7
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-10 23:18:15 +1000

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -11,13 +11,16 @@ export default function Home() {
       
       <main className={styles.main}>
         <div>
-          <h1 className={styles.name}>Luke Reynolds |&nbsp;</h1>
-          <a className={styles.links} href="https://www.github.com/lreynolds188" target="_blank">GITHUB.COM/LREYNOLDS188</a>
+          <div>
+            <h1 className={styles.name}>Luke Reynolds |&nbsp;</h1>
+            <a className={styles.links} href="thereynolds.com.au" target="_blank">THEREYNOLDS.COM.AU</a>
+          </div>
+          <p className={styles.contactDeets}>QUEENSLAND, AU | M: 0420 619 829</p>
           <br />
-          <p className={styles.contactDeets}>QUEENSLAND, AU | M: 0420 619 829 | E:&nbsp;</p>
+          <p className={styles.contactDeets}>E:&nbsp;</p>
           <a className={styles.contactDeets} href="mailto: luke@thereynolds.com" target="_blank">LUKE@THEREYNOLDS.COM.AU</a>
           <p className={styles.contactDeets}>&nbsp;| W:&nbsp;</p>
-          <a className={styles.contactDeets} href="thereynolds.com.au" target="_blank">THEREYNOLDS.COM.AU</a>
+          <a className={styles.contactDeets} href="https://www.github.com/lreynolds188" target="_blank">GITHUB.COM/LREYNOLDS188</a>
     
           <h2 className={styles.categoryHeaders}>Summary</h2>
           <hr />
@@ -38,6 +41,7 @@ export default function Home() {
           <h2 className={styles.categoryHeaders}>Education</h2><hr />
           <a className={styles.primaryTitles} href="http://qut.edu.au" target="_blank">Queensland University of Technology | Queensland, AU</a>
           <p className={styles.categoryDetails}>Bachelor of Computer Science<br/>Gardens Point Campus<br/>2017-2020</p>
+          <br />
           <a className={styles.primaryTitles} href="http://tafebrisbane.edu.au" target="_blank">Tafe Queensland | Queensland, AU</a>
           <p className={styles.categoryDetails}>Diploma of Software Development<br/>Loganlea Campus<br/>2016</p>
                              
@@ -45,21 +49,20 @@ export default function Home() {
           <p className={styles.primaryTitles}>Ridgeline Manufacturing | Queensland, AU</p>
           <p className={styles.categoryDetails}>Project/Operations Manager, May 2020 - Present</p>
           <p className={styles.categoryDescription}>While in this position I was granted the tools to achieve a task and a large amount of freedom to self manage which allowed to me to excel. I was responsible for seeing that operations run as normal and look to for any opportunities for improvement. In addition to these duties I was also required to design and implement an automation system for one of the companies reactors.</p>
-
+          <br />
           <a className={styles.primaryTitles} href="https://www.kilcoyglobalfoods.com/" target="_blank">Kilcoy Global Foods | Queensland, AU</a>
           <p className={styles.categoryDetails}>Developer/Technician, Nov 2018 – May 2019</p>
           <p className={styles.categoryDescription}>During my time in this position I was responsible for the development of business applications and support of a production environment. Projects were completed in a full stack agile development environment in which version control and project management tools were utilized.</p>
-
+          <br />
           <a className={styles.primaryTitles} href="http://www.actlogistics.com.au/" target="_blank">ACT Logistics / ACT Computers | Queensland, AU</a>
           <p className={styles.categoryDetails}>Technician, Aug 2014 – Apr 2015</p>
           <p className={styles.categoryDescription}>In this position I worked in a computer store and distribution warehouse managing the redistribution of ex-government hardware. My position included such jobs as system reloads, stripping hardware, technical support, computer & phone repairs, and on-site support.</p>
-
+          <br />
           <a className={styles.primaryTitles} href="https://ozecomputers.com.au/#Computer%20Repairs%20Caloundra" target="_blank">OzeComputers | Queensland, AU</a>
           <p className={styles.categoryDetails}>Technician, Aug 2013 – Aug 2014</p>
           <p className={styles.categoryDescription}>During my time at this position I ran a computer store in the center of Caloundra upholding the highest in customer service in delivery of all services. I was responsible for running diagnostics, computer & phone repairs, delivering on-site support, building computer systems, server configuration and support.</p>
           
           <h2 className={styles.categoryHeaders}>Skills</h2><hr />
-          <br />
           <h3 className={styles.primaryTitles}>Languages & Frameworks</h3>
           <p className={styles.categoryDescription}>C | C# | .NET | Python | Java | JavaScript | VisualBasic | ASP | SQL | HTML | CSS | PHP </p>
           <h3 className={styles.primaryTitles}>Tools & Practices</h3>
