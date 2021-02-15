1fe981364d9bc2722a7cf1e3cd34750488a25b20
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-15 19:41:13 +1100

    Update nextcloud-server.js

--- pages/tutorials/nextcloud-server.js
+++ pages/tutorials/nextcloud-server.js
@@ -11,7 +11,6 @@ export default function Home() {
     	<meta name="author"      content="Luke Reynolds">
     	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
     	<meta name="keywords"    content="Nextcloud, Ubuntu">
-    	<link rel="icon"      href="/favicon.ico">
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
