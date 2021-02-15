48e7570436d60d54157d0f1d281c4ec466365b8e
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-15 19:58:09 +1100

    Update tutorials.js

--- pages/tutorials.js
+++ pages/tutorials.js
@@ -11,41 +11,16 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image
-          src="/profile.jpg"
-          alt="Luke Reynolds"
-          width={180}
-          height={180}
-        />
-
-        <h1 className={styles.title}>
-          Luke Reynolds
-        </h1>
+        <h1 className={styles.title}>Tutorials</h1>
 
         <div className={styles.grid}>
           <a href="/nextcloud-server" className={styles.card}>
             <h3>Nextcloud Server Ubuntu 20.04 &rarr;</h3>
-            <p>In this tutorial I will explain how to setup a Nextcloud server featuring an NginX reverse proxy, vnc remote maintenance, 2 factor-authentication, and Android connectivity</p>
+            <p>In this tutorial I will explain how to setup a Nextcloud server featuring an NginX reverse proxy, 2-FA, Android connectivity, and VNC for remote maintenance</p>
           </a>
 
-          <a href="/resume" className={styles.card}>
-            <h3>Resume &rarr;</h3>
-            <p>See past education and work experience.</p>
-          </a>
-
-          <a href="https://github.com/lreynolds188" className={styles.card}>
-            <h3>GitHub &rarr;</h3>
-            <p>Discover my past, current, and future projects on GitHub.</p>
-          </a>
-    
-          <a href="/tutorials.js" className={styles.card}>
-            <h3>Tutorials &rarr;</h3>
-            <p>Read some tutorials i've developed.</p>
-          </a>
-    
-          <a href="/quotes" className={styles.card}>
-            <h3>Quotes</h3>
-            <p>View some quotes i've collected over the years.</p>
+          <a href="/moretocome" className={styles.card}>
+            <h3>More coming soon! &rarr;</h3>
           </a>
         </div>
       </main>
