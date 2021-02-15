4365fc90649ce804c0938f2939908d47a0e10cee
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-15 22:46:18 +1000

    Update nextcloud-server.js

--- pages/tutorials/nextcloud-server.js
+++ pages/tutorials/nextcloud-server.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Luke Reynolds</title>
 	<meta charset="utf-8" />
-	<meta name="description" content="In my desire to regain information privacy from big tech and their incessant tracking I have set up a Nextcloud server so that I may find some peace of mind and help others to find the same. The server features a domain name, Nginx reverse proxy, SSL encryption, Android connectivity, and VNC for remote maintenance." />
+	<meta name="description" content="In my desire to regain information privacy from big tech and their incessant tracking I have set up a Nextcloud server so that I may find some peace of mind and help others to do the same. The server features a domain name, Nginx reverse proxy, SSL encryption, Android connectivity, and VNC for remote maintenance." />
     	<meta name="author" content="Luke Reynolds" />
     	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     	<meta name="keywords" content="Nextcloud, Ubuntu" />
@@ -17,12 +17,12 @@ export default function Home() {
       <main className={styles.main}>
 	  <div>
 	  	<h1 className={styles.title}>Nextcloud Server - Ubuntu 20.04</h1>
-			<p>Luke Reynolds, 15/02/21</p>
-			<p>In my desire to regain information privacy from big tech and their incessant tracking I decided to set up a Nextcloud server so that I may find some peace of mind and help others to find the same. The server features a domain name, Nginx reverse proxy, SSL encryption, Android connectivity, and VNC for remote maintenance.</p>
+			<p>15/02/21, Luke Reynolds</p>
+			<p>In my desire to regain information privacy from big tech and their incessant tracking I decided to set up a Nextcloud server so that I may find some peace of mind and help others to do the same. The server features a domain name, Nginx reverse proxy, SSL encryption, Android connectivity, and VNC for remote maintenance.</p>
 			<strong>Requirements:</strong>
 			<ol>
 			  <li>A computer to use as a server (I'm using a SurfacePro 3).</li>
-			  <li>A static IP or DynamicDNS</li>
+			  <li>A static IP and domain name or DynamicDNS</li>
 			  <li>Access to your router's control panel</li>
 			</ol>
 
@@ -33,12 +33,15 @@ export default function Home() {
 				<p>There will be 2 virtual machines that will be responsible for hosting your server. The first of these machines will store your Nextcloud instance, while the second will store the NginX reverse proxy. I decided to install Ubuntu 20.04 on my host machine but you may use whatever OS you prefer. Once the server has a fresh OS installation, install VirtualBox or your preferred virtualization software and download the latest Ubuntu Server image (.iso). </p>
 				<p>VirtualBox can be downloaded <a href="https://www.virtualbox.org/wiki/Downloads">here</a></p>
 				<p>Ubuntu Server can be downloaded <a href="https://ubuntu.com/download/server">here</a> (Option: 2)</p>
+			<br />
 				<p>Once the virutalization software is installed create 2 new virtual machines. Set both the server hard drives to dynamically allocated, giving the NginX server a max of 8gb and the Nextcloud server whatever remaining space is available preferably leaving around 10gb free for the host OS.</p>
 				<p>After creating the virtual machines, go into the network settings of each and set the mode to "bridged". This sets the machines as independent hosts on the network allowing visibility.</p>
 			<h3>Nextcloud Server Setup</h3>
 				<p>Then, start up the nextcloud server and select the ubuntu server image when prompted for a startup disk. This will load up the ubuntu live CD and go through the installation steps. The only extra step to do during the installation is to select the nextcloud snap when prompted with commonly installed snaps. After the installation is finished, nextcloud is pretty much done and ready to use on the LAN! To try it out, log in to the machine and find out its ip with ifconfig. Go to that ip in a web browser and you should be greeted with a nextcloud welcome page prompting you to set up an administrator account. You might as well do that while you’re there.</p>
-	  		<h3>Reverse Proxy Server Setup</h3>
-				<p>Start up the reverse proxy server and go through the motions of installing ubuntu server, this time not selecting any snaps when prompted. Once logged in, install nginx if necessary (it was already installed on my image). Then, find the ip of this server with ifconfig and take a note of it (I’ll refer to it as nginx-ip, and the ip of the nextcloud server as nextcloud-ip). Then, make a file at /etc/nginx/sites-available/nextcloud.conf and fill it as follows; don’t forget to replace the ’s with their real values!</p>
+	  		<br />
+			<h3>Reverse Proxy Server Setup</h3>
+				<p>Start up the reverse proxy server and go through the motions of installing ubuntu server, this time not selecting any snaps when prompted. Once logged in, install nginx by typing <code>sudo apt install nginx</code>. Then, find the IP of this server with ifconfig and take a note of it (I’ll refer to it as nginx-ip, and the ip of the nextcloud server as nextcloud-ip). Then, make a file at /etc/nginx/sites-available/nextcloud.conf and fill it as follows; don’t forget to replace the ’s with their real values!</p>
+			<br />
 			<h3>Setting Static IP's for the VM's</h3>
 				<p>To set a static IP on each of the virtual machines boot them up and run this command to install net-tools.</p>
 				<pre><code>sudo apt install net-tools</code></pre>
@@ -49,6 +52,7 @@ export default function Home() {
 		<h2>Exposing Server to the Internet</h2>
 			<h3>Port Forwarding</h3>
 				<p>In your router’s settings, configure port 80 to forward all traffic to the NginX server and port 443 to forward TCP traffic to the NginX server. For additional help refer to your router's online manual.</p>
+			<br />
 			<h3>Static IP or Dynamic DNS</h3>
 				<strong>Option 1: Static IP & Domain Name</strong>
 				<p>Configure A records in your domain's DNS configuration portal to point at your static IP address. For help finding your public IP address click <a href="https://www.whatismyip.com">here</a></p>
@@ -68,8 +72,29 @@ export default function Home() {
 				<p>Then, reinstall the proxy config</p>
 				<code>sudo ln -s /etc/nginx/sites-available/nextcloud.conf /etc/nginx/sites-enabled/</code>
 				<p>and edit it to be like this:</p>
+				<pre><code>
+					server &#123;
+						listen 80;
+						server_name example.com;
+						return 301 https://$server_name:443$request_uri;
+					&#125;
+					
+					server 
+						listen 443 ssl;
+						server_name example.com;
+	
+						ssl_certificate /etc/letsencrypt/live/example.com/fullchain.pem;
+						ssl_certificate_key /etc/letsencrypt/live/example.com/privkey.com;
+	
+						add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains; preload';
+						add_header X-XSS-Protection "1; mode=block" always;
+						add_header X-Frame-Options "SAMEORIGIN" always;
+						add_header X-Content-Type-Options "nosniff" always;
+						add_header X-Permitted-Cross-Domain
+					</code></pre>
 				<p>Finally, restart nginx</p>
 				<pre><code>sudo service nginx restart</code></pre>
+			<br />
 			<h3>Automatic Certificate Renewal</h3>
 				<p>The ssl certificates expire every 90 days, but they can be easily and non-interactively renewed with</p>
 				<pre><code>sudo certbot renew</code></pre>
@@ -78,8 +103,9 @@ export default function Home() {
 				<p>Adding the line</p>
 				<pre><code>0 0 1 */2 * /usr/bin/certbot -q renew</code></pre>
 				<p>Which will automatically renew the certificates at midnight on the first of every other month.</p>
+			<br />
 			<h3>Enable 2-Factor Authentication</h3>
-				<p>Install FreeOTP+ from <a href="https://f-droid.org/en/packages/org.liberty.android.freeotpplus/">F-Droid</a> or the<a href="https://play.google.com/store/apps/details?id=org.liberty.android.freeotpplus&hl=en_US&gl=US">Playstore</a> or your chosen 2FA code generator.</p>
+				<p>Install FreeOTP+ from <a href="https://f-droid.org/en/packages/org.liberty.android.freeotpplus/">F-Droid</a> or the <a href="https://play.google.com/store/apps/details?id=org.liberty.android.freeotpplus&hl=en_US&gl=US">Playstore</a> or your chosen 2FA code generator.</p>
 	
 		<h2>RealVNC Server Setup</h2>
 			<p>RealVNC Server can be found <a href="https://www.realvnc.com/en/connect/download/vnc/">here</a></p>
@@ -90,8 +116,9 @@ export default function Home() {
 		<h2>Android Connectivity</h2>
 			<h3>Nextcloud App Code</h3>
 				<p>Navigate to your Nextcloud portal, go to settings -> personal -> Security. Scroll to the bottom of the page and type the name of the app you would like to grant access to. Click create new app password. Go back to the application you want to connect to your Nextcloud server, type in your username and the 29 character one time app password.</p>
+			<br />
 			<h3>Task, Contact, and Calendar Syncronization Client</h3>
-				<p>Install DAVx⁵ from <a href="https://f-droid.org/en/packages/at.bitfire.davdroid/">F-Droid</a> or the<a href="https://play.google.com/store/apps/details?id=at.bitfire.davdroid&hl=en_US&gl=US">Playstore</a>.</p>
+				<p>Install DAVx⁵ from <a href="https://f-droid.org/en/packages/at.bitfire.davdroid/">F-Droid</a> or the <a href="https://play.google.com/store/apps/details?id=at.bitfire.davdroid&hl=en_US&gl=US">Playstore</a>.</p>
 		<br />	
 				
 		<h2>References</h2>
