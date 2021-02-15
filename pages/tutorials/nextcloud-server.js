26115bce10804a1a4e15f5dc5ad454b8403f178e
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-15 20:10:06 +1100

    Update nextcloud-server.js

--- pages/tutorials/nextcloud-server.js
+++ pages/tutorials/nextcloud-server.js
@@ -34,27 +34,7 @@ export default function Home() {
 		
 	  	<h2>Reverse proxy server setup</h2>
 		<p>Next, start up the reverse proxy server and go through the motions of installing ubuntu server, this time <span class="underline">not</span> selecting any snaps when prompted. Once logged in, install <code>nginx</code> if necessary (it was already installed on my image). Then, find the ip of this server with <code>ifconfig</code> and take a note of it (I&rsquo;ll refer to it as <code>nginx-ip</code>, and the ip of the nextcloud server as <code>nextcloud-ip</code>). Then, make a file at <code>/etc/nginx/sites-available/nextcloud.conf</code> and fill it as follows; don&rsquo;t forget to replace the &rsquo;s with their real values!</p>
-		<pre><code>
-	  	sudo vi /etc/nginx/sites-available/nextcloud.conf
-		
-		server 
-			The IP that you forwarded in your router (nginx proxy)
-		  listen &lt;nginx-ip&gt;:80 default_server;
-
-			The internal IP of the VM that hosts your Apache config
-			set $upstream &lt;nginx-ip&gt;;
-		location / 
-			proxy_headers_hash_max_size 512;
-			proxy_headers_hash_bucket_size 64;
-			proxy_set_header Host $host;
-			proxy_set_header X-Forwarded-Proto $scheme;
-			proxy_set_header X-Real-IP $remote_addr;
-			proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-			add_header Front-End-Https on;
-				whatever the IP of your cloud server is
-			proxy_pass http://&lt;nextcloud-ip&gt;;
-			
-		</code></pre>
+		<code>sudo vi /etc/nginx/sites-available/nextcloud.conf</code></pre>
 
 		<p>Then symlink that config to <code>/etc/nginx/sites-enabled/</code> and restart nginx:</p>
 		<pre><code>sudo ln -s /etc/nginx/sites-available/nextcloud.conf /etc/nginx/sites-enabled/
@@ -106,38 +86,13 @@ export default function Home() {
 		<p>This is actually pretty easy to do thanks to <a href="https://letsencrypt.org/">Let&rsquo;s Encrypt</a>. First, port forwarding needs to be set up on port 443 because that&rsquo;s the port used for ssl. This was already done in the port forwarding section above.</p>
 		<p>The next step is to obtain ssl certificates, which is also pretty easy. The certificates need to be set up on the nginx server, because that will be the terminal for ssl connections. So log into the nginx server and install Let&rsquo;s Encrypt&rsquo;s certbot following <a href="https://certbot.eff.org/lets-encrypt/ubuntuartful-nginx">the installation instructions on the website</a>.</p>
 		<p>Before running <code>certbot</code>, however, I needed to disable the reverse proxy traffic forwarding of the nginx server. To do this, just disable the proxy config and restart nginx before running certbot.</p>
-		<pre><code>sudo rm /etc/nginx/sites-enabled/nextcloud.conf
+		<code>sudo rm /etc/nginx/sites-enabled/nextcloud.conf
 		sudo service nginx restart
-		sudo certbot --nginx</code></pre>
+		sudo certbot --nginx</code>
 		<p>Then, reinstall the proxy config</p>
-		<pre><code>sudo ln -s /etc/nginx/sites-available/nextcloud.conf /etc/nginx/sites-enabled/</code></pre>
+		<code>sudo ln -s /etc/nginx/sites-available/nextcloud.conf /etc/nginx/sites-enabled/</code>
 		<p>and edit it to be like this:</p>
-		<pre><code>server 
-			The IP that you forwarded in your router (nginx proxy)
-		  listen &lt;nginx-ip&gt;:443 ssl ipv6only=on;
-		  server_name &lt;your-domain.url&gt;;
-		  listen 443 ssl; 
-		  ssl_certificate /etc/letsencrypt/live/&lt;your-domain.url&gt;/fullchain.pem;
-		  ssl_certificate_key /etc/letsencrypt/live/&lt;your-domain.url&gt;/privkey.pem;
-		  include /etc/letsencrypt/options-ssl-nginx.conf;
-		  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
-
-			The internal IP of the VM that hosts your Apache config
-			set $upstream &lt;nginx-ip&gt;;
-		location / 
-			proxy_headers_hash_max_size 512;
-			proxy_headers_hash_bucket_size 64;
-			proxy_set_header Host $host;
-			proxy_set_header X-Forwarded-Proto $scheme;
-			proxy_set_header X-Real-IP $remote_addr;
-			proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-			add_header Front-End-Https on;
-				whatever the IP of your cloud server is
-				Note that it's forwarding traffic to port 80 because the nextcloud server
-				is only set up for http (not ssl)
-        		proxy_pass http://&lt;nextcloud-ip&gt;:80;
-        		
-		</code></pre>
+		<code>set $upstream</code>
 
 		<p>Finally, restart nginx</p>
 		<pre><code>sudo service nginx restart</code></pre>
