68c285fc89c1a9ebe01fdfdde39967407749c9b0
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-18 18:54:36 +1000

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -13,7 +13,7 @@ export default function Home() {
         <div>
           <div>
             <h1 className={styles.name}>Luke Reynolds |&nbsp;</h1><a className={styles.contactDeets} href="https://goo.gl/maps/uz7s2XdAUTmpstBA8" target="_blank">QUEENSLAND, AU</a><br />
-            <p className={styles.contactDeets}><a className={styles.links} href="mailto:luke@thereynolds.com.au" target="_blank">LUKE</a> [AT] <a className={styles.links} href="https://thereynolds.com.au" target="_blank">THEREYNOLDS.COM.AU</a></p>
+            <p className={styles.contactDeets}>LUKE [AT] <a className={styles.links} href="https://thereynolds.com.au" target="_blank">THEREYNOLDS.COM.AU</a></p>
           </div>
 
           <h2 className={styles.categoryHeaders}>Summary</h2>
