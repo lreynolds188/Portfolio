3cb2c6c7c85677b08a2bbf59b4c22f5333871e78
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-18 20:16:43 +1000

    Update nextcloud-server.js

--- pages/tutorials/nextcloud-server.js
+++ pages/tutorials/nextcloud-server.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
-import '../../styles/globals.css'
+import styles from '../../styles/globals.css'
 
 export default function Home() {
   return (
