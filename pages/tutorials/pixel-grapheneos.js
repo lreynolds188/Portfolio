3261844c5e3be80aa271b709e17536ea7843908e
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-02-20 21:05:57 +1000

    Update pixel-grapheneos.js

--- pages/tutorials/pixel-grapheneos.js
+++ pages/tutorials/pixel-grapheneos.js
@@ -11,7 +11,7 @@ export default function Home() {
       
       <main className={styles.main}>  
         <h1 className={styles.title}>Pixel GrapheneOS Installation</h1>
-	  	<div>
+	  	<div className={styles.descriptionLeft}>
 		  	<br />
 			<p>00/00/0000, <a href="https://thereynolds.com.au">Luke Reynolds</a></p>
 			<p className={styles.description}>Coming soon...</p><br /><br />
