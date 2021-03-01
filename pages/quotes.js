0600d3f9bb202e0b44f366214f9cd5dab97b48a2
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-03-01 19:58:47 +1000

    Update quotes.js

--- pages/quotes.js
+++ pages/quotes.js
@@ -147,7 +147,7 @@ export default function Home() {
 				</div>
 				<div className={styles.picture}>
 	  			<Image
-				  src="/locke.jpg"
+				  src="https://en.wikipedia.org/wiki/John_Locke#/media/File:John_Locke.jpg"
 				  alt="Locke"
 				  width={180}
 				  height={180}
