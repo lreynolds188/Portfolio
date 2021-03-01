8e45b435293889d0b8c8743b75ce03d5f0118589
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-03-01 19:04:37 +1000

    Update quotes.js

--- pages/quotes.js
+++ pages/quotes.js
@@ -18,7 +18,7 @@ export default function Home() {
 				  width={180}
 				  height={180}
 				/>
-	 			<div>
+	 			<div style="display:inline-block;vertical-align:top;>
 				<h3>Socrates (Philosopher, 470 - 399 BC)</h3>
             				<ul className={styles.descriptionLeft}>
 						<li>Strong minds discuss ideas, average minds discuss events, and weak minds discuss people.</li>
