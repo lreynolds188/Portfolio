25b6a7e17f200a99ce872ca071c1f9345a425f25
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-12 16:17:15 +1000

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -19,7 +19,7 @@ export default function Home() {
           <h2 className={styles.categoryHeaders}>Summary</h2>
           <hr />
           <p className={styles.description}>
-            I am a Computer Scientist and owner of <a href="https://automatedtechnicalsolutions.com">Automated Tech Solutions</a>
+            I am a Computer Scientist and owner of <a href="https://automatedtechnicalsolutions.com">Automated Tech Solutions</a>&nbsp;
             a technical consulting firm based in Queensland, Australia.
             I currently have one subject remaining to receive my degree and am the sole member of my business
             working to implement infrastructure and build the customer base.
