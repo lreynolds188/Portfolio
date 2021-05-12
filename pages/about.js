32511ec890d42528c7c9218172ff67b48d02df69
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-12 15:40:57 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -14,10 +14,13 @@ export default function Home() {
         <div>
         <br />
          <p className={styles.description}>
-            I am a Computer Scientist currently employed at Ridgeline Manufacturing as a Project and Operations Manager.
-            I am responsible for maintaining the production environment and designing and implenting an automation system for the company's main chemical reactor.
+            I am a Computer Scientist and owner of <a href="https://automatedtechnicalsolutions.com">Automated Tech Solutions</a> a technical consulting firm.
+            I currently still have one subject remaining on my degree and am the sole member of my business
+            working to implement infrastructure and build the customer base.
           </p><br /><br />
           <p className={styles.description}>
+            In 2021 I was employed as a Project and Operations manager responsible for supporting the production environment 
+            and implementing an IoT automation system for the company's primary reactor. 
             In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud
             based portal for QUT. In 2018 and 2019 I was responsible for developing business applications and supporting
             the production environment of a large food processing plant. In 2016 I completed a Diploma of Software Development
@@ -25,8 +28,8 @@ export default function Home() {
           </p><br /><br />
           <p className={styles.description}>
             Upon completion of my degree I plan to have a solid foundation in a range of differnet development
-            languages and techniques, with a keen interest on cyber security, microsystems, and artificial intelligence.
-            My hope is to work advancing the functionality, affordability, and sustainability of futuristic technological applications.
+            languages and techniques, with a keen interest in cyber security, microsystems, and IoT automation devices.
+            My hope is to work toward advancing the functionality, affordability, and sustainability of futuristic technological applications.
           </p>
         </div>
       </main>
