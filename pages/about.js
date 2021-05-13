9353ce856d84e0af4b98578e0728dc99c2d84203
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-13 12:56:34 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -16,19 +16,17 @@ export default function Home() {
          <p className={styles.description}>
             I am a Computer Scientist and the owner of <a href="https://automatedtechnicalsolutions.com">Automated Tech Solutions</a> a technical consulting firm based in Queensland, Australia.
             I currently still have one subject remaining to complete my degree and am the sole member of my business working to implement infrastructure and build the customer base.
-          </p><br /><br />
-          <p className={styles.description}>
+            <br /><br />
             At the beginning of my career I worked as a computer technician for a range of different companies between 2012 and 2015.
             In 2016 I received a diploma of software development from Tafe Queensland. In 2018 and '19 I was employed by a large food 
             processing plant and responsible for developing business applications and supporting the production environment.
             In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud based portal for QUT.
             In my last position as a project and operations manager I was responsible for supporting the production environment 
             and implementing an IoT automation system for the company's primary reactor.
-          </p><br /><br />
-          <p className={styles.description}>
+            <br /><br />
             Upon completion of my degree I plan to have a solid foundation in a range of different development
             languages and techniques with a keen focus on cyber security, blockchain technology, and IoT automation devices. In addition to a
-            basic foundation of mathematics, physics, business, psychology, and philosophy.
+            basic foundation of mathematics, physics, business, economics, psychology, and philosophy.
             <br/><br/>
             My goal is to work toward advancing the functionality, affordability, and sustainability of futuristic technological applications.
           </p>
