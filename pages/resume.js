082082a91eb92533f274288c53e917b5176a52f8
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-13 12:28:40 +1000

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -22,12 +22,12 @@ export default function Home() {
             I am a Computer Scientist and the owner of <a href="https://automatedtechnicalsolutions.com">Automated Tech Solutions</a> a technical consulting firm based in Queensland, Australia.
             I currently have one subject remaining to receive my degree and am the sole member of my business working to implement infrastructure and build the customer base.
             <br/><br/>
-            In 2021 I was employed as a project and operations manager responsible for supporting the production environment 
-            and implementing an IoT automation system for the company's primary reactor. 
-            In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud
-            based portal for QUT. In 2018 and 2019 I was responsible for developing business applications and supporting
-            the production environment of a large food processing plant. In 2016 I completed a diploma of software development
-            at Tafe Queensland and from 2012 to 2015 was employed as a computer technician.
+            At the beginning of my career I worked as a computer technician for a range of different companies between 2012 and 2015.
+            In 2016 I received a diploma of software development from Tafe Queensland. In 2018 and '19 I was employed by a large food 
+            processing plant and responsible for developing business applications and supporting the production environment.
+            In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud based portal for QUT.
+            In my last position I a project and operations manager responsible for supporting the production environment 
+            and implementing an IoT automation system for the company's primary reactor.
             <br/><br/>
             Upon completion of my degree I plan to have a solid foundation in a range of different development
             languages and techniques with a keen focus on cyber security, blockchain technology, and IoT automation devices. In addition to a
