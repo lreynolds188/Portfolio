76a22af5c23b169999c28191e4e76505937121ee
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-16 16:23:51 +1000

    Update quotes.js

--- pages/quotes.js
+++ pages/quotes.js
@@ -292,6 +292,7 @@ export default function Home() {
               <li>The weak can never forgive. Forgiveness is the attribute of the strong.</li>
               <li>Strength does not come from physical capacity. It comes from an indomitable will.</li>
               <li>Happiness is when what you think, what you say, and what you do are in harmony.</li>
+              <li>First they ignore you, then they laugh at you, then they fight you, then you win.</li>
             </ul>
           </div><br/><br />
           <div className={styles.picture}>
