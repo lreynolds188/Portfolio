2585d466e17d4bce5e49aa7a1216988de5ab92d0
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2023-10-21 21:13:53 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -14,16 +14,9 @@ export default function Home() {
         <div>
         <br />
          <p className={styles.description}>
-            At the start of my career I worked as a computer technician for a range of different companies between 2012 and 2015.
-            In 2016 I received a diploma of software development from Tafe Queensland. In 2018 and '19 I was employed by a large food 
-            processing plant and was responsible for developing business applications and supporting the production environment.
-            In 2020 I was the project manager for a university based start-up with the goal of implementing a cloud based portal for QUT.
-            In my last position as a project and operations manager I was responsible for implementing an IoT automation system for the 
-            company's primary concentrator and supporting the production environment.  In 2021 I received a Bachelor's Degree in 
-            Computer Science from the Queensland University of Technology.
-            <br /><br />
-            I have a solid foundation in a range of different development languages and techniques with a keen interest in privacy & security, 
-            blockchain technology, and IoT automation devices. My goal is to work in automation and online privacy.
+            At the start of my career I worked as a Computer Technician for a range of different companies between 2012 and 2015. In 2016 I received a Diploma of Software Development from TAFE Queensland. In 2018 and '19 I was employed by a large food processing plant and was responsible for developing business applications and supporting the production environment. In 2020 I was the Project Manager for a university based start-up with the goal of implementing a cloud based AI integrated portal for QUT. In my last position as a Project & Operations Manager I was responsible for implementing an IoT automation system for the company's primary concentrator and supporting the production environment. In 2021 I received a Bachelor's Degree in Computer Science from the Queensland University of Technology. I am currently working as an Uber Eats contractor and running my own tech support business and online wiki.
+
+            I have a solid foundation in a range of different development languages and techniques with a keen interest in privacy, security, open source software, blockchain technology, and IoT devices.
           </p>
         </div>
       </main>
